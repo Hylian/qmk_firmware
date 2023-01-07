d98582f858d8b23941b1f3072d54941d43642428
Edward Shin <contact@edwardsh.in>
2023-01-06 20:21:28 -0500

    Add ergodox ez support

--- layouts/community/ergodox/edward/config.h
+++ layouts/community/ergodox/edward/config.h
@@ -1,17 +1,22 @@
 #pragma once
 
+#undef  MOUSEKEY_INTERVAL
 #define MOUSEKEY_INTERVAL       20
+#undef  MOUSEKEY_DELAY
 #define MOUSEKEY_DELAY          0
+#undef  MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX    60
+#undef  MOUSEKEY_MAX_SPEED
 #define MOUSEKEY_MAX_SPEED      7
+#undef  MOUSEKEY_WHEEL_DELAY
 #define MOUSEKEY_WHEEL_DELAY    0
 
 /* Set 0 if debouncing isn't needed */
 #undef DEBOUNCE
 #define DEBOUNCE  3
 
+#ifdef ST7565_ENABLE
 #define SPLIT_LAYER_STATE_ENABLE
-//#define SPLIT_ST7565_ENABLE
 #define ST7565_TIMEOUT 0
-
 #define usb_lld_disconnect_bus(usbp) do {} while(0)
+#endif
